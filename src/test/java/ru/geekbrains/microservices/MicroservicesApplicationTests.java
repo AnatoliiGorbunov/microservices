d4fcd062c1dec7a18b338780b2303303182bada8
Aanatolii Gorbunov <617299@bk.ru>
2022-08-29 17:08:21 +0300

    Initial commit

---
+++ src/test/java/ru/geekbrains/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
